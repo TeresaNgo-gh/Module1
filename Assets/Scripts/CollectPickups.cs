d6534479ac738c9009c1eb8e7fc0c431a7223e32
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-14 15:52:35 -0500

    removed all collectible levels and had to re-do because the pickup wasn't made a prefab

---
+++ Assets/Scripts/CollectPickups.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectPickups : MonoBehaviour{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    void OnCollisionEnter(Collision coll){
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag =="Pickup"){
+            Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 1;
+            scoreGT.text = score.ToString();
+        }
+    }
+}
