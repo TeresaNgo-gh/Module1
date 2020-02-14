cc4fa5268d909580a7eca1fe3a3f5e4a2c0a352c
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-14 14:27:05 -0500

    now instantiate a projectile when mouse is down

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slingshot : MonoBehaviour
-{
+public class Slingshot : MonoBehaviour{
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake(){
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
     void OnMouseEnter(){
@@ -18,5 +25,12 @@ public class Slingshot : MonoBehaviour
 
     void OnMouseExit(){
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown(){
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
