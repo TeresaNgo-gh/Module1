5590f5d60c169304073030b779a49d93fa70a7e2
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-14 14:42:07 -0500

    can now launch projectile, but keeps hitting slingshot

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour{
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
 
     [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+
+    private Rigidbody projectilRigidbody;
 
     void Awake(){
         Transform launchPointTrans = transform.Find("LaunchPoint");
@@ -32,5 +35,34 @@ public class Slingshot : MonoBehaviour{
         projectile = Instantiate(prefabProjectile) as GameObject;
         projectile.transform.position = launchPos;
         projectile.GetComponent<Rigidbody>().isKinematic = true;
+
+        projectilRigidbody = projectile.GetComponent<Rigidbody>();
+        projectilRigidbody.isKinematic = true;
+    }
+
+    void Update(){
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude){
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0)){
+            aimingMode = false;
+            projectilRigidbody.isKinematic = false;
+            projectilRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 }
