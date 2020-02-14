c9254e81f9cf47761e9b5835fc3d7b27c294e9f8
TeresaNgo-gh <57918864+TeresaNgo-gh@users.noreply.github.com>
2020-02-14 17:03:51 -0500

    added a game manager script to the main camera that will instantiate the collecibles and change levels

--- Assets/Scripts/GameMgmt.cs
+++ Assets/Scripts/GameMgmt.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameMgmt : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameMgmt : MonoBehaviour{
+    static private GameMgmt S;
+
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Vector3 collectiblesPos;
+    public GameObject[] collectibles;
+
+    [Header("Set Dynamically")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject collectible;
+
+    void Start(){
+        S = this;
+        level = 0;
+        levelMax = collectibles.Length;
+        StartLevel();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void StartLevel(){
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach(GameObject pTemp in gos){
+            Destroy(pTemp);
+        }
+
+        collectible = Instantiate<GameObject>(collectibles[level]);
+        collectible.transform.position = collectiblesPos;
+        shotsTaken = 0;
+    }
+
+    void UpdateGUI(){
+        uitLevel.text = "Level: "+(level+1)+" of "+levelMax;
+        uitShots.text = "Shots Taken: "+shotsTaken;
+    }
+
+    void Update(){
+        UpdateGUI();
+    }
+
+    void NextLevel(){
+        level++;
+        if(level == levelMax){
+            level=0;
+        }
+        StartLevel();
+    }
+
+    public static void ShotFired(){
+        S.shotsTaken++;
     }
 }
